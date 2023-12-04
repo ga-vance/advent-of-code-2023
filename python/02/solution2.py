f80988fe96e330e9893de6185c221ebc24f9f9d2
ga-vance <71193788+ga-vance@users.noreply.github.com>
2023-12-04 10:37:44 -0700

    Completed Day 2 solutions

--- python/02/solution2.py
+++ python/02/solution2.py
@@ -1,5 +1,22 @@
 def main():
-    pass
+    with open("input.txt") as input:
+        total = 0
+        for game, line in enumerate(input):
+            maximums = {"red": 0, "green": 0, "blue": 0}
+            check = {}
+            draws = line.split(":")[1].strip().split("; ")
+            for draw in draws:
+                colors = draw.split(', ')
+                for color in colors:
+                    hand = color.split()
+                    check[hand[1]] = int(hand[0])
+                for key in check:
+                    maximums[key] = max(maximums[key], check[key])
+            power = 1
+            for key in maximums:
+                power *= maximums[key] 
+            total += power
+        print(total)
 
 if __name__ == "__main__":
     main()
